38feabceb9f869e941db4cc80cf3acc3d0b35912
John Frommeyer <johnfr@seas.upenn.edu>
2023-03-10 13:25:26 -0500

    Use queue name to get url

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "service_lambda" {
       PENNSIEVE_DOMAIN      = data.terraform_remote_state.account.outputs.domain_name,
       REGION                = var.aws_region
       RDS_PROXY_ENDPOINT    = data.terraform_remote_state.pennsieve_postgres.outputs.rds_proxy_endpoint,
-      RESTORE_PACKAGE_QUEUE = aws_sqs_queue.restore_package_queue.arn
+      RESTORE_PACKAGE_QUEUE = aws_sqs_queue.restore_package_queue.name
     }
   }
 }
