665eb4590578882408841b217c08bc246b20ae17
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2025-11-01 09:30:17 -0400

    fix yml file

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -13,19 +13,19 @@ resource "aws_apigatewayv2_api" "packages_service_api" {
   body = templatefile("${path.module}/packages-service.yml", {
     authorize_lambda_invoke_uri    = data.terraform_remote_state.api_gateway.outputs.authorizer_lambda_invoke_uri
     gateway_authorizer_role        = data.terraform_remote_state.api_gateway.outputs.authorizer_invocation_role
-    collections_service_lambda_arn = aws_lambda_function.service_lambda.arn
+    packages_service_lambda_arn = aws_lambda_function.service_lambda.arn
   })
 }
 
 resource "aws_apigatewayv2_api_mapping" "packages_service_api_map" {
   api_id          = aws_apigatewayv2_api.packages_service_api.id
   domain_name     = var.api_domain_name
-  stage           = aws_apigatewayv2_stage.collections_service_gateway_stage.id
+  stage           = aws_apigatewayv2_stage.packages_service_gateway_stage.id
   api_mapping_key = "packages"
 
 }
 
-resource "aws_apigatewayv2_stage" "collections_service_gateway_stage" {
+resource "aws_apigatewayv2_stage" "packages_service_gateway_stage" {
   api_id = aws_apigatewayv2_api.packages_service_api.id
 
   name        = "$default"
@@ -50,7 +50,7 @@ resource "aws_apigatewayv2_stage" "collections_service_gateway_stage" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "collections_service_integration" {
+resource "aws_apigatewayv2_integration" "packages_service_integration" {
   api_id             = aws_apigatewayv2_api.packages_service_api.id
   integration_type   = "AWS_PROXY"
   connection_type    = "INTERNET"
@@ -58,7 +58,7 @@ resource "aws_apigatewayv2_integration" "collections_service_integration" {
   integration_uri    = aws_lambda_function.service_lambda.invoke_arn
 }
 
-resource "aws_lambda_permission" "collections_service_lambda_permission" {
+resource "aws_lambda_permission" "packages_service_lambda_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.service_lambda.function_name
