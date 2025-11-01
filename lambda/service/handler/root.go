20a3a556ee91d36ef138a74e3c17a89d3924d210
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2025-11-01 10:50:39 -0400

    fix path name --> remove packages prefix

--- lambda/service/handler/root.go
+++ lambda/service/handler/root.go
@@ -1,18 +1,18 @@
 package handler
 
 import (
-	"context"
-	"github.com/aws/aws-lambda-go/events"
-	"net/http"
+    "context"
+    "github.com/aws/aws-lambda-go/events"
+    "net/http"
 )
 
 func (h *RequestHandler) handle(ctx context.Context) (*events.APIGatewayV2HTTPResponse, error) {
 
-	switch h.path {
-	case "/packages/restore":
-		restoreHandler := RestoreHandler{RequestHandler: *h}
-		return restoreHandler.handle(ctx)
-	default:
-		return h.logAndBuildError("resource not found: "+h.path, http.StatusNotFound), nil
-	}
+    switch h.path {
+    case "/restore":
+        restoreHandler := RestoreHandler{RequestHandler: *h}
+        return restoreHandler.handle(ctx)
+    default:
+        return h.logAndBuildError("resource not found: "+h.path, http.StatusNotFound), nil
+    }
 }
